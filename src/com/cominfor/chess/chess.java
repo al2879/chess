3badb1432c03c7ca9f503f20ecfe2d7d70721c2b
uskov <uskov@cominfor.ru>
2014-01-18 19:44:59 +0400

    Chess 18.01.2014 done with "greedy" algorithm. Graphics commented out.

--- src/com/cominfor/chess/chess.java
+++ src/com/cominfor/chess/chess.java
@@ -1,15 +1,17 @@
 package com.cominfor.chess;
 
+import com.sun.javafx.collections.transformation.SortedList;
 import sun.java2d.loops.FillRect;
 
 import java.awt.*;
+import java.awt.List;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Created by Admin on 17.12.13.
@@ -17,23 +19,16 @@ import java.util.Date;
 public class chess {
 
     private static boolean[][] board;
-    private static long[] run_stat;
-    private static final int BOARD_SIZE = 4;
+    private static final int BOARD_SIZE = 8;
     private static final int RUN_LENGTH = BOARD_SIZE*BOARD_SIZE;
     private static int runp = 0;
-    private static int max_runp = 0;
     public cell start_cell;
-    /*Integer.MAX_VALUE =  2147483647
-    Integer.MIN_VALUE = -2147483648
-
-    Long.MAX_VALUE =  9223372036854775807
-    Long.MIN_VALUE = -9223372036854775808*/
     public long moves = 0;
-    public long moves_multiplyer = 0;
 
-    public class cell{
+    public class cell implements Comparable<cell>{
         private int row;
         private int column;
+        private int power;
 
         public boolean get(){
             return board[column][row];
@@ -44,9 +39,38 @@ public class chess {
         public void set_false(){
             board[column][row] = false;
         }
-        public cell(int columnv, int rowv) {
-            row = rowv;
-            column = columnv;
+        public int get_power(){
+            return this.power;
+        }
+        public cell(int column, int row) {
+            this.row = row;
+            this.column = column;
+            this.power = 0;
+            if (check_cell(column, row)) {
+                if (check_cell(column - 1, row - 2)) this.power++;
+                if (check_cell(column + 1, row - 2)) this.power++;
+                if (check_cell(column + 2, row - 1)) this.power++;
+                if (check_cell(column + 2, row + 1)) this.power++;
+                if (check_cell(column + 1, row + 2)) this.power++;
+                if (check_cell(column - 1, row + 2)) this.power++;
+                if (check_cell(column - 2, row + 1)) this.power++;
+                if (check_cell(column - 2, row - 1)) this.power++;
+            } else this.power = 100;
+        }
+        public boolean check_cell(int column, int row){
+            if(column < 0      || column > (BOARD_SIZE-1) ||
+                    row < 0    || row > (BOARD_SIZE-1))
+                return false;
+            return board[column][row];
+        }
+
+        @Override
+        public int compareTo(cell o) {
+            int retval = 0;
+            if(power <  o.power) retval = -1;
+            if(power == o.power) retval =  0;
+            if(power >  o.power) retval =  1;
+            return retval;
         }
     }
 
@@ -58,42 +82,41 @@ public class chess {
     }
 
     public void prdebug(int n, cell c){
-        String outs="(%d,%d)%2d:";
+        String outs="(%d,%d)%2d:p(%d)";
         for(int i=0; i<n; i++) outs += "+";
         outs += "\n";
-        System.out.format(outs,c.column,c.row,n);
+        System.out.format(outs,c.column,c.row,n,c.get_power());
     }
 
     public boolean gorun(cell nc){
-        cell[] mvs = {  new cell(nc.column-1,nc.row-2),
-                        new cell(nc.column+1,nc.row-2),
-                        new cell(nc.column+2,nc.row-1),
-                        new cell(nc.column+2,nc.row+1),
-                        new cell(nc.column+1,nc.row+2),
-                        new cell(nc.column-1,nc.row+2),
-                        new cell(nc.column-2,nc.row+1),
-                        new cell(nc.column-2,nc.row-1)
+
+        cell [] mvs = {
+        new cell(nc.column - 1, nc.row - 2),
+        new cell(nc.column + 1, nc.row - 2),
+        new cell(nc.column + 2, nc.row - 1),
+        new cell(nc.column + 2, nc.row + 1),
+        new cell(nc.column + 1, nc.row + 2),
+        new cell(nc.column - 1, nc.row + 2),
+        new cell(nc.column - 2, nc.row + 1),
+        new cell(nc.column - 2, nc.row - 1)
         };
+        java.util.List<cell> newmvs = Arrays.asList(mvs);
+        java.util.Collections.sort(newmvs);
+        /*System.out.println("--------------------------------------------------");
+        for(cell i: newmvs){
+            System.out.println(String.format("%d:%d:%d", i.power, i.column, i.row ));
+        }*/
         runp++;
+//        prdebug(runp, nc);
         moves++;
-        run_stat[runp-1]++;
-        if(run_stat[runp-1] == 2){
-            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-            System.out.format("Down Max run:%2d, Timestamp:%s,Moves:%d\n",runp,timeStamp,moves);
-        }
         nc.set_false();
         if(runp >= RUN_LENGTH){
             System.out.println("("+nc.column+","+nc.row+")");
             return true;
         };
-        if(runp > max_runp){
-            max_runp++;
-            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-            System.out.format("Max run:%2d, Timestamp:%s,Moves:%d\n",max_runp,timeStamp,moves);
-        }
-        for(int i=0; i<mvs.length; i++)
-            if(check_cell(mvs[i])){
-                if(gorun(mvs[i])){
+        for(cell i: newmvs)
+            if(check_cell(i)){
+                if(gorun(i)){
                     System.out.println("("+nc.column+","+nc.row+")");
                     return true;
                 };
@@ -105,8 +128,6 @@ public class chess {
 
     public chess() {
         board = new boolean[BOARD_SIZE][BOARD_SIZE];
-        run_stat = new long[RUN_LENGTH];
-        for (int i=0; i<RUN_LENGTH; i++) run_stat[i]=0;
         for (int i=0; i<BOARD_SIZE; i++)
             for (int j=0; j<BOARD_SIZE; j++) board[i][j]= true;
         start_cell = new cell(0,0);
@@ -131,7 +152,7 @@ public class chess {
         System.out.println("Time in seconds: " + diffSeconds + " seconds.");
         System.out.println("Time in minutes: " + diffMinutes + " minutes.");
         System.out.println("Time in hours: " + diffHours + " hours.");
-        final Frame f = new Frame("Draw string")  {
+        /*final Frame f = new Frame("Draw string")  {
             public void paint (Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
 
@@ -148,9 +169,9 @@ public class chess {
         f.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-                /*int keycode = e.getKeyCode();
+                int keycode = e.getKeyCode();
                 System.out.println("kc:"+keycode);
-                if(keycode== 100) f.dispose();*/
+                if(keycode== 100) f.dispose();
             }
 
             @Override
@@ -192,6 +213,6 @@ public class chess {
             }
         });
         f.setVisible(true);
-        f.setSize(550, 550);
+        f.setSize(550, 550)*/;
     }
 }
